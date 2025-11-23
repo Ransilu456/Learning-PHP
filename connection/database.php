ba7219c7a0d7a7ac2e926631fe50ea51b40502d3
Ransilu456 <samarasekarakeshan@gmail.com>
2025-11-23 07:22:24 +0530

    Database Conection Created

--- connection/database.php
+++ connection/database.php
@@ -1,4 +1,23 @@
 <?php
-   echo("Database Connection file.")
+echo ("Database Connection file.");
 
-?>
+// Database Configuration
+$host = "localhost";
+$database_name = "phptest";
+$database_Username = "root";
+$database_Password = "";
+
+// Create the connection here
+$connection = new mysqli($host, $database_Username, $database_Password, $database_name);
+
+// Check the connection
+if ($connection->connect_error) {
+    echo ("Connection Failed: " . $connection->connect_error);
+    die("Connection Failed: " . $connection->connect_error);
+}
+
+// For every input unicode
+$connection->set_charset("utf+");
+
+//Database File Ended
+echo("Database code Ended here");
